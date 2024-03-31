733ac1ccbd91604ecadfdd77f31b4e300db60589
Paulo Busch <paulo.busch@codhalum.com.br>
2024-03-30 21:07:28 -0300

    feat: solution and api

---
+++ src/Sample.Api/Controllers/SampleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sample.Api.Controllers
+{
+    [ApiController]
+    [Route("sample")]
+    public class SampleController : ControllerBase
+    {
+        public string Today() => DateTime.Now.ToString("dd/MM/yyyy");
+    }
+}
